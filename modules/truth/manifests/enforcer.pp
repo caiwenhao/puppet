d7fb1c20f9981477550d952dc375617f250e3a8d
caiwenhao <caiwenhao186@live.com>
2014-09-29 08:52:49 +0800

    logrotate

--- modules/truth/manifests/enforcer.pp
+++ modules/truth/manifests/enforcer.pp
@@ -15,6 +15,7 @@ class truth::enforcer {
           sshd_password_authentication  => 'no',
           sshd_config_use_dns           => 'no',
           hiera_merge                   => true,
+	  sshd_config_loglevel          => 'VERBOSE',
     }
 
    class { 'dnsclient':
@@ -89,10 +90,57 @@ firewall { '0009 accept ping':
 	ensure => 'present',
 	password => sha1($uuid),
 }
+    package { [ "augeas-devel",
+                "augeas" ]:
+        ensure => "present",
+        allow_virtual => false,
+    }
 
+class {'augeasproviders::instances':
+	sysctl_hash => { 
+        'net.ipv4.ip_forward' => { 'value' => '0' },
+        'net.ipv4.conf.default.rp_filter' => { 'value' => '1' },
+        'net.ipv4.conf.default.accept_source_route' => { 'value' => '0' },
+        'kernel.sysrq' => { 'value' => '0' },
+        'kernel.core_uses_pid' => { 'value' => '1' },
+        'net.ipv4.tcp_syncookies' => { 'value' => '1' },
+        'kernel.msgmnb' => { 'value' => '0' },
+        'kernel.msgmax' => { 'value' => '65536' },
+        'kernel.shmmax' => { 'value' => inline_template("<%= memorysize_mb.to_i*1024*1024 %>") },
+	'kernel.shmall' => { 'value' => inline_template("<%= memorysize_mb.to_i*1024/4 %>") },
+        'kernel.shmmni' => { 'value' => '4096' },
+        'net.nf_conntrack_max' => { 'value' => '655360' },
+        'net.netfilter.nf_conntrack_max' => { 'value' => '655360' },
+        'net.netfilter.nf_conntrack_tcp_timeout_established' => { 'value' => '1200' },
+        'net.ipv4.tcp_window_scaling' => { 'value' => '1' },
+	'net.ipv4.tcp_sack' => { 'value' => '1' },
+	'net.ipv4.tcp_timestamps' => { 'value' => '0' },
+	'net.ipv4.tcp_synack_retries' => { 'value' => '2' },
+	'net.ipv4.tcp_syn_retries' => { 'value' => '2' },
+	'net.ipv4.tcp_tw_reuse' => { 'value' => '1' },
+	'net.ipv4.tcp_tw_recycle' => { 'value' => '1' },
+	'net.ipv4.ip_local_port_range' => { 'value' => '1024 65000' },
+	'net.ipv4.tcp_max_syn_backlog' => { 'value' => '8192' },
+	'net.ipv4.ip_local_reserved_ports' => { 'value' => '3306,4369,8000-8300,9000-9300,20000-30000,61618' },
+	'net.ipv4.tcp_fin_timeout' => { 'value' => '30' },
+	'net.ipv4.tcp_retries2' => { 'value' => '5' },
+	'net.ipv4.tcp_max_tw_buckets' => { 'value' => '180000' },
+	'net.core.somaxconn' => { 'value' => '1024' },
+	'kernel.core_pattern' => { 'value' => '/data/tmp/core' },
+	'net.nf_conntrack_max' => { 'value' => '655360' },
+	'net.netfilter.nf_conntrack_max' => { 'value' => '655360' },
+	'net.netfilter.nf_conntrack_tcp_timeout_established' => { 'value' => '1200' },
+	},
 
+}
 
-
+logrotate::rule { 'messages':
+  path         => '/var/log/cron /var/log/maillog /var/log/messages /var/log/secure /var/log/spooler',
+  rotate       => 5,
+  rotate_every => 'week',
+  sharedscripts => true,
+  postrotate   => '/bin/kill -HUP `cat /var/run/syslogd.pid 2> /dev/null` 2> /dev/null || true',
+}
 
     notify {"I am a database":}
   } else {
